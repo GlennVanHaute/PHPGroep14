d3173cf2831f42d6b72f74d58aa48539386a53a0
LisaSmets <lisa_smets@hotmail.com>
2014-05-02 12:29:16 +0200

    Menu aanmaken ok

--- classes/nieuwMenu.class.php
+++ classes/nieuwMenu.class.php
@@ -1,7 +1,12 @@
 <?php
+
+include_once('db.php');
+
 class nieuwMenu
 {
 	private $m_snieuwMenu;	
+	private $m_snieuwMenuDetails;
+	private $m_inieuwMenuPrijs;	
 	public function __set($p_sProperty, $p_vValue)
 	{
 		switch($p_sProperty)
@@ -12,6 +17,10 @@ class nieuwMenu
 
 			case "details":
 				$this->m_snieuwMenuDetails = $p_vValue;
+				break;
+
+			case "prijs":
+				$this->m_inieuwMenuPrijs = $p_vValue;
 				break;
 		}	   
 	}
@@ -28,22 +37,45 @@ class nieuwMenu
 			case "details": 
 				$vResult = $this->m_snieuwMenuDetails;
 				break;
+
+			case "prijs": 
+				$vResult = $this->m_inieuwMenuPrijs;
+				break;
 		}
 		return $vResult;
 	}
 
 	public function Save()
 	{
-
-		include_once('db.php');
 		$db = new Db();
-		$sql = "INSERT INTO nieuwMenu(Naam, Details) 
+		$sql = "INSERT INTO nieuwMenu(Naam,Details,prijs) 
 				VALUES (
-					'".	$db->conn->real_escape_string($this->Naam)."',
-					'".	$db->conn->real_escape_string($this->Details)."'
+					'".	$db->conn->real_escape_string($this->naam)."',
+					'".	$db->conn->real_escape_string($this->details)."',
+					'".	$db->conn->real_escape_string($this->prijs)."'
 					)";	
 		$db->conn->query($sql);
+		
+		//print_r($sql);
+	}
+
+	public function getAll()
+	{
+
+		$db = new Db();
+		$sql = "select * from nieuwMenu";
+    	return $db->conn->query($sql);
+	}
+
+	public function getGerecht()
+	{
+
+		$db = new Db();
+		$sql = "select * from nieuwMenu where Naam =";
+    	return $db->conn->query($sql);
 	}
 }
+
+
 
 ?>
