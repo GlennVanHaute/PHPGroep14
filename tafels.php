64faae40c300b06424d4b37e72e1550070f2233d
LisaSmets <lisa_smets@hotmail.com>
2014-05-10 17:18:12 +0200

    tafels.php reservatiebevestiging ok
    Tafels = pagina waarop gereserveerd wordt

--- tafels.php
+++ tafels.php
@@ -1,7 +1,9 @@
-<?php include_once('classes/db.php')?>
-<?php $db = new Db(); ?>
+<?php 
 
-<?php
+	include_once('classes/db.php');
+	$db = new Db();
+
+
 	if (!empty($_POST)) 
 	{
 
@@ -24,13 +26,26 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	
 	<meta charset="UTF-8">
 	<title>Reservatie</title>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script>
+		$(document).ready(function(){
+			$("#resbevtitel").hide();
+			$("#reserveerprint").on('click', function(){
+				$("#resbevtitel").show();
+				$("#resbevtitel").html('<h2>Deze reservatie was succesvol:</h2>');
+
+				var text = $(this).prev().text();
+				$("#resbev").text(text);
+			});
+		});
+	</script>
 </head>
 <body>
 
-		<?php include_once('classes/nav_include.php') ?>
+<?php include_once('classes/nav_include.php') ?>
 
 
 <h1>RESERVATIE</h1>
@@ -66,10 +81,14 @@
 	if (!empty($_POST)) 
 	{
 
-		echo "<h3> Tafels van " . $aantal . " personen:</h3>";
+		echo "<h2> Tafels van " . $aantal . " personen:</h2>";
 
 		
-		if(mysqli_num_rows($check) == 0)
+		if(mysqli_num_rows($check) ==0 && mysqli_num_rows($check2)== 0)
+		{
+			echo "<p>Er zijn geen tafels voor " .$aantal. " personen. U kan wel meerdere kleine tafels reserveren.";
+		}
+		else if(mysqli_num_rows($check) == 0)
 		{
 			echo "<p> Er is geen tafel vrij voor exact " . $aantal . " personen.
 			Kijk hieronder voor andere reserveerbare tafels voor meer dan " . $aantal . " personen</p>";
@@ -99,7 +118,7 @@
 		
 		if(mysqli_num_rows($check2) != 0)
 		{
-			echo "<h3> Tafels die u ook kan reserveren:</h3>";
+			echo "<h2> Tafels die u ook kan reserveren:</h2>";
 
 			foreach ($result2 as $tafel) 
 			{
@@ -119,7 +138,7 @@
 				}
 			}
 		}
-
+		
 		
 	}
 	
@@ -132,7 +151,7 @@
 if (empty($_POST)) 
 {
 	$sql3 = "select * from tafelbeheer order by Tafelnummer";
-	echo"<h3>Alle tafels van dit restaurant</h3>";
+	echo"<h2>Alle tafels van dit restaurant</h2>";
 
 	$result3 = $db->conn->query($sql3);
 	foreach ($result3 as $tafel) 
@@ -148,22 +167,14 @@ if (empty($_POST))
 ?> 
 </ul>
 
-<section id="reservatiebevestiging">
+<div id='resbevtitel'></div>
+<div id="resbev"></div>
 
-	<script>
-		$(document).ready(function(){
-			$("#reserveerprint").on('click', function(){
-				var text = $(this).prev().text();
 
-				$("#reservatiebevestiging").text(text);
-			});
-		});
-	</script>
 
-<!--<?php include('reservatie.php') ?>
-//seservatie moet $tafelr $datum $uur pakken en in reservatie steken.-->
+<!-- reservatie moet $tafelr $datum $uur pakken en in reservatie steken. -->
 
-</section>
+
 
 	
 </body>
