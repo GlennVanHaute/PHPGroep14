71384bfe39566cf25aa17844f6d7c2f34b9201f2
GlennVanHaute <glennvanhaute@gmail.com>
2014-05-11 17:52:50 +0200

    sql added

---
+++ sql/tblusers.sql
@@ -0,0 +1,46 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.9
+-- http://www.phpmyadmin.net
+--
+-- Machine: 127.0.0.1
+-- Genereertijd: 11 mei 2014 om 17:07
+-- Serverversie: 5.6.14
+-- PHP-versie: 5.5.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Databank: `restoapp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `tblusers`
+--
+
+CREATE TABLE IF NOT EXISTS `tblusers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Gegevens worden uitgevoerd voor tabel `tblusers`
+--
+
+INSERT INTO `tblusers` (`id`, `name`, `email`, `password`) VALUES
+(1, 'glenn', 'glenn@hotmail.com', '20735262ac36152d7ce07afde819a460');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
