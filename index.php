d455a63f4cc84ce6b86d2d32e4fa3b5e2bf11372
GlennVanHaute <glennvanhaute@gmail.com>
2014-05-12 17:22:43 +0200

    minor changes

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
 	<title>Table4You</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/style.css">
+
 	<link rel="stylesheet" href="css/slicknav.css">
+		<link rel="stylesheet" href="css/style.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
     <script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
 </head>
--- register.php
+++ register.php
@@ -42,6 +42,43 @@ if (isset($_POST['btnSignup'])) {
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href="style.css" media="all">
+	
+	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function(){
+		console.log('ready');
+		$("#username").on("blur", function(e){
+					//var clickedLink = $(this);
+					console.log('BLURED');
+					var user = $("#username").val();
+					console.log(user);
+					$(".usernameFeedback").css("display","block");
+					$.ajax({
+					  type: "POST",
+					  url: "ajax/check_username.php",
+					  data: { user: user }
+					})
+					  .done(function(result) {
+					    console.log(result);
+					    if(result == 'true')
+					    {
+					    	$(".usernameFeedback").html("<p id='available'>Yup :), username is available!</p>");
+					    }
+					    else
+					    {
+					    	$(".usernameFeedback").html("<p id='notavailable'>:( sorry, username is already taken!</p>");
+					    }
+					  });
+
+					e.preventDefault();
+				});
+
+		$("#username").on("focus", function(e){
+			$(".feedback").css("display","none");
+			$(".error").css("display","none");
+		});
+	});
+	</script>
 </head>
 <body>
 	<div class="container">
