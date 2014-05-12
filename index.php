9180125dbd1b2544f67238756f45436c57573c9e
Dorien Thys <dorien.thys1@gmail.com>
2014-05-12 14:14:59 +0200

    NAV
    Nav pagina's kloppen maar moet mobile worden.

--- index.php
+++ index.php
@@ -6,18 +6,22 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
 </head>
 <body>
+<? session_start(); ?>
 
-	<div class="container">
+<?php include('nav_include.php') ?>
 
+	<div class="header">
 		<div class="row">
 			<div class="col-md-12">
 				
+				<img src="images/logo.jpg" class="logo" alt="table4you"/>
 
 			</div>			
 		</div>
-
 
 	</div>
 
